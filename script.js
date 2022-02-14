84609839b8bfe27eb81591bd6e831fc80cf5e738
Timur Mishiev <timurmishiev@timurs-mbp.myfiosgateway.com>
2022-02-14 17:38:22 -0500

    create list of priorities

---
+++ script.js
@@ -0,0 +1,18 @@
+const draggable_list = document.getElementById("draggable-list");
+const check = document.getElementById("check");
+
+const priorityList = [
+	"Brush teeth",
+	"Exersize",
+	"Take a shower",
+	"Drink Coffee",
+	"Learn JS",
+	"Take a break",
+	"Learn Algorithms",
+	"Have a dinner with Ira",
+	"Watch a movie",
+	"Listen to a Lecture",
+];
+
+//Store the list items
+const listItems = [];
